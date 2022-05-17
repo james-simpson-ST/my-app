7220cec2660011b2dc0f3ecc1f0828785a931beb
James Simpson <james.simpson@trustpayments.com>
2022-05-17 16:48:30 +0100

    Update to add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,12 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "exampletable": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import awsconfig from "../../aws-exports";
+import AWS from "aws-sdk";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  AWS.config.update({ region: awsconfig.aws_project_region });
+
+  // Create the DynamoDB service object
+  var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+  var params = {
+    TableName: "exampletable",
+    Item: {
+      note: { S: "Example" },
+    },
+  };
+  ddb.putItem(params, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+  res.status(200).json({ name: "John Doe" });
 }
