80304a986fe2160391c4c24b70b001367d0cf217
James Simpson <james.simpson@trustpayments.com>
2022-05-18 09:16:39 +0100

    Add aws sdk

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,25 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-//import awsconfig from "../../aws-exports";
-//import AWS from "aws-sdk";
+import awsconfig from "../../aws-exports";
+import AWS from "aws-sdk";
 
 export default function handler(req, res) {
-  //AWS.config.update({ region: awsconfig.aws_project_region });
+  AWS.config.update({ region: awsconfig.aws_project_region });
 
   // Create the DynamoDB service object
-  //var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+  var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
-  //var params = {
-  //  TableName: "exampletable",
-  //  Item: {
-  //    note: { S: "Example" },
-  //  },
-  //};
-  //ddb.putItem(params, function (err, data) {
-  //  if (err) {
-  //    console.log("Error", err);
-  //  } else {
-  //    console.log("Success", data);
-  //  }
-  //});
-  res.status(200).json({ name: "John Doe" });
+  var params = {
+    TableName: "exampletable",
+    Item: {
+      note: { S: "Example" },
+    },
+  };
+  let result;
+  ddb.putItem(params, function (err, data) {
+    if (err) {
+      result = err.message;
+      console.log("Error", err);
+    } else {
+      result = "success";
+      console.log("Success", data);
+    }
+  });
+  res.status(200).json({ message: result });
 }
