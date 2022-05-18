68a328a4ba55ec54d89408b7ba78c0725c540ded
James Simpson <james.simpson@trustpayments.com>
2022-05-18 09:31:15 +0100

    Logging

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -3,17 +3,21 @@ import awsconfig from "../../aws-exports";
 import AWS from "aws-sdk";
 
 export default function handler(req, res) {
-  AWS.config.update({ region: awsconfig.aws_project_region });
+  console.log("1");
+  AWS.config.update({
+    region: awsconfig.aws_dynamodb_table_schemas[0].region,
+  });
 
   // Create the DynamoDB service object
   var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
-
+  console.log("2");
   var params = {
-    TableName: "exampletable",
+    TableName: awsconfig.aws_dynamodb_table_schemas[0].tableName,
     Item: {
       note: { S: "Example" },
     },
   };
+  console.log(`3 ${awsconfig.aws_dynamodb_table_schemas[0].tableName}}`);
   let result;
   ddb.putItem(params, function (err, data) {
     if (err) {
@@ -24,5 +28,6 @@ export default function handler(req, res) {
       console.log("Success", data);
     }
   });
+  console.log(`4 ${result}}`);
   res.status(200).json({ message: result });
 }
