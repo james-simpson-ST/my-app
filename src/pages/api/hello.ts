7e1a25f73f0ca2938cd3d13110b6259179550468
James Simpson <james.simpson@trustpayments.com>
2022-07-04 10:49:18 +0100

    Import axios in API

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 const Example = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(axios.VERSION);
   res.status(200).json({ hello: "world" });
 };
 export default Example;
